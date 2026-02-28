9a803f1428cbaa945bc85842010dfe4daddcac45
rbc33 <ricardobenthem@gmail.com>
2026-02-28 22:16:25 +0100

    done

--- src/iteration2.ts
+++ src/iteration2.ts
@@ -3,3 +3,16 @@
 //
 
 
+const calcMultiplication = (a:number, b: number) => {
+    return a * b
+}
+
+
+const isEven = (n:number) => {
+    return n % 2 === 0
+}
+
+const calcArrayAverage = (numbersArr: number[]) => {
+    const sum = numbersArr.reduce((acc, num) => acc + num, 0)
+    return sum / numbersArr.length
+}
--- src/iteration3.ts
+++ src/iteration3.ts
@@ -1,5 +1,33 @@
 //
 // Iteration 3 | Classes
 //
+class BankAccount {
+    balance: number = 0
+    accountHolder: string;
+
+    constructor (accountHolderName: string) {
+        this.accountHolder = accountHolderName
+    }
+
+    getBalance(): number{
+        return this.balance
+    }
+    deposit(amount: number):string|number{
+        if (amount <= 0) {
+            return "Please provide a valid amount"
+        }
+        this.balance += amount
+        return this.balance
+    }
+    withdraw(amount: number): number|string{
+        if (amount > 0 && amount < this.balance) {
+            this.balance -= amount
+            return this.balance
+        }
+
+        return "Insuficient funds"
+    
+    }
+}
 
 
